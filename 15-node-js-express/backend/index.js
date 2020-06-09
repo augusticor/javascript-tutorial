dcccb3459443cc376504a8239574862faaa428ab
Oscar Rojas <rojascruzoscar@gmail.com>
2020-06-09 12:21:23 -0500

    Removing bad coffee jokes and changing the location of css and js file into Html

--- 15-node-js-express/backend/index.js
+++ 15-node-js-express/backend/index.js
@@ -1,15 +1,14 @@
 'use strict';
-const cafecito_express = require('express');
-const server = cafecito_express();
+const express = require('express');
+const server = express();
 
 server.set('appName', 'Sending form data with nodeJS and express');
 server.set('port', 3000);
 
-server.use(cafecito_express.static('../frontend'));
-server.use(cafecito_express.json());
+server.use(express.static('../frontend'));
+server.use(express.json());
 
 server.post('/submit-form', (request, response) => {
-    console.log('I got a request');
     console.log(request.body);
     response.json({
         status: 'Success !!',
@@ -23,5 +22,5 @@ server.post('/submit-form', (request, response) => {
 
 server.listen(server.get('port'), () => {
     console.log(server.get('appName'));
-    console.log(`Now my cafecito server is listening on port ${server.get('port')}`);
+    console.log(`Now Express JS is listening on port ${server.get('port')}`);
 });
